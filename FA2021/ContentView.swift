1c2012dce44627599e8c1dc3d698d1e03b645c18
Kevin Huang <kevin.huang@tum.de>
2021-09-19 23:16:21 +0200

    Initial Commit

---
+++ FA2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FA2021
+//
+//  Created by Kevin Huang on 19.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FA2021/FA2021App.swift
@@ -0,0 +1,17 @@
+//
+//  FA2021App.swift
+//  FA2021
+//
+//  Created by Kevin Huang on 19.09.21.
+//
+
+import SwiftUI
+
+@main
+struct FA2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
