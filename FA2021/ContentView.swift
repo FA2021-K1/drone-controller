0573f0759e6b8c5777e3a5f8fcdae27ea1ec4bbf
Kevin Huang <kevin.huang@tum.de>
2021-09-21 09:04:52 +0200

    Add initial project configuration (Pod and first waypoint experiments)

--- FA2021/ContentView.swift
+++ FA2021/ContentView.swift
@@ -6,11 +6,49 @@
 //
 
 import SwiftUI
+import DJISDK
 
 struct ContentView: View {
+    var missionControl = MissionScheduler()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            Button {
+                DJISDKManager.missionControl()?.scheduleElement(DJITakeOffAction())
+                
+            } label: {
+                Text("Takeoff").padding(20)
+            }.contentShape(Rectangle())
+            
+            Button {
+                guard let mission = missionControl.createDemoMission()
+                else {
+                    return
+                }
+                
+                DJISDKManager.missionControl()?.scheduleElement(mission)
+                
+            } label: {
+                Text("Start Mission").padding(20)
+            }.contentShape(Rectangle())
+            
+            
+            Button {
+                DJISDKManager.missionControl()?.scheduleElement(DJILandAction())
+                
+            } label: {
+                Text("Land").padding(20)
+            }.contentShape(Rectangle())
+            
+            
+            Button {
+                DJISDKManager.missionControl()?.unscheduleEverything()
+                DJISDKManager.missionControl()?.scheduleElement(DJILandAction())
+                
+            } label: {
+                Text("Force Land").padding(20)
+            }.contentShape(Rectangle())
+        }
     }
 }
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FA2021' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FA2021
+  pod 'DJI-SDK-iOS', '~> 4.15.1'
+
+  target 'FA2021Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FA2021UITests' do
+    # Pods for testing
+  end
+
+end
