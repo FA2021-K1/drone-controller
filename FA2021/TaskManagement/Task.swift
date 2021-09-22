de84af03f83ebb834536b42bf3db10d706444bc9
Armin Ettenhofer <armin.ettenhofer@tum.de>
2021-09-22 14:17:16 +0200

    Added capability to parse a tasks from a json

--- FA2021/TaskManagement/Task.swift
+++ FA2021/TaskManagement/Task.swift
@@ -1,39 +1,52 @@
+import Foundation
+
+enum TaskType: String, Decodable {
+    case FlyToTask, GetDataTask, NonTerminalTask
+}
+
 class Task {
-    
     let id: String
     let name: String
     let type: TaskType
-    let drone_id: String
-    
-    internal init(id: String, name: String, type: TaskType, drone_id: String) {
+
+    init(id: String, name: String, type: TaskType) {
         self.id = id
         self.name = name
         self.type = type
-        self.drone_id = drone_id
     }
 }
 
-class FlyToTask:Task {
+class FlyToTask: Task {
     let latitude: Double
     let longitude: Double
     let altitude: Double
-    
-    internal init(id: String, name: String, type: TaskType, drone_id: String, latitude: Double, longitude: Double, altitude: Double) {
+
+    init(id: String, name: String, type: TaskType, latitude: Double, longitude: Double, altitude: Double) {
         self.latitude = latitude
         self.longitude = longitude
         self.altitude = altitude
-        super.init(id: id, name: name, type: type, drone_id: drone_id)
+        super.init(id: id, name: name, type: type)
+    }
+}
+
+class NonTerminalTask: Task {
+    let tasks: [Task]
+
+    init(id: String, name: String, type: TaskType, tasks: [Task]) {
+        self.tasks = tasks
+        super.init(id: id, name: name, type: type)
     }
 }
 
 class GetDataTask: Task {
 }
 
-class NonTerminalTask:Task {
-    internal init(id: String, name: String, type: TaskType, drone_id: String, tasks: [Task]) {
-        self.tasks = tasks
-        super.init(id: id, name: name, type: type, drone_id: drone_id)
-    }
-    
-    let tasks: [Task]
+class UnknownTask: Decodable {
+    let id: String
+    let name: String
+    let type: TaskType
+    let latitude: Double?
+    let longitude: Double?
+    let altitude: Double?
+    let tasks: [UnknownTask]?
 }
--- FA2021/TaskManagement/TaskManager.swift
+++ FA2021/TaskManagement/TaskManager.swift
@@ -1,27 +1,69 @@
-enum TaskType {
-	case FlyToTask, GetDataTask, NonTerminalTask
-}
+import Foundation
 
 struct TaskManager {
+    func parseJsonToTasks(json: String) -> [Task] {
+        guard let data = json.data(using: .utf8) else {
+            return []
+        }
+        var unknown_tasks: [UnknownTask] = []
+        do {
+            unknown_tasks = try! JSONDecoder().decode([UnknownTask].self, from: data)
+        }
+        var tasks: [Task] = []
+        for unknown_task in unknown_tasks {
+            tasks.append(parseTask(unknown: unknown_task))
+        }
 
-    
-    /**
-        @return tasks waiting to be assigned
-     */
-    func retrieveAvailableTasks() -> [Task] {
-        // allTasksAvilableOrInProgress   coaty?: get/observe all tasks in network
-        // dronesWorkingOnTaskSinceTimestamp    coaty? (if not coaty add to json): get all drones that work on task with timestamp
-        // filter allTasksAvilableOrInProgress with dronesWorkingOnTaskSinceTimestamp
-        return [];
-	}
+        return tasks
+    }
+
+    func parseTask(unknown: UnknownTask) -> Task {
+        let id: String = unknown.id
+        let name: String = unknown.name
+        let type: TaskType = unknown.type
+        let task: Task
+
+        switch type {
+        case .FlyToTask:
+            task = FlyToTask(id: id, name: name, type: type,
+                    latitude: unknown.latitude ?? 0,
+                    longitude: unknown.longitude ?? 0,
+                    altitude: unknown.altitude ?? 0)
+
+        case .GetDataTask:
+            task = GetDataTask(id: id, name: name, type: type)
+
+        case .NonTerminalTask:
+            var tasks: [Task] = []
+            if let unknown_tasks: [UnknownTask] = unknown.tasks {
+                for unknown_task in unknown_tasks {
+                    tasks.append(parseTask(unknown: unknown_task))
+                }
+            } else {
+                tasks = []
+            }
+            task = NonTerminalTask(id: id, name: name, type: type, tasks: tasks)
+        }
+        return task
+    }
 
     /**
      gets called while tasks available
      */
-    func aquireTask(){
+    func acquireTask() {
         let availableTask = retrieveAvailableTasks()
         for task in availableTask {
-            
+
         }
+    }
+
+    /**
+    @return tasks waiting to be assigned
+    */
+    func retrieveAvailableTasks() -> [Task] {
+        // allTasksAvailableOrInProgress   coaty?: get/observe all tasks in network
+        // dronesWorkingOnTaskSinceTimestamp    coaty? (if not coaty add to json): get all drones that work on task with timestamp
+        // filter allTasksAvailableOrInProgress with dronesWorkingOnTaskSinceTimestamp
+        return [];
     }
 }
