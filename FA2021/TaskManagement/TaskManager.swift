b8ff94ba7b3eaf2ffcf4e44ef54b7f77f70c3f96
schusterbenjamin <benjamin.schuster.dorfen@web.de>
2021-09-22 17:57:50 +0200

    add busy waiting to scan for task

--- FA2021/TaskManagement/TaskManager.swift
+++ FA2021/TaskManagement/TaskManager.swift
@@ -5,7 +5,7 @@ struct TaskRegistration {
     let droneId: String
     let timestamp: TimeInterval
     let status: Any
-
+    
     internal init(taskId: String, droneId: String, timestamp: TimeInterval, status: Any) {
         self.taskId = taskId
         self.droneId = droneId
@@ -15,83 +15,129 @@ struct TaskRegistration {
 }
 
 class TaskManager {
-
-    let currentTasks: [Task]
-
+    
+    var currentTasks: [Task]
+    let droneId: String = "abcdefg" // TODO: find out from where to get droneId
+    
     init() {
         currentTasks = []
         // TODO: registerForUnfinishedTasks() // api call to syncLibrary
     }
-
-    func unfinishedTasksChanged(unfinishedTasks: [Task]){
+    
+    /**
+     entry point
+     */
+    func scanForTask(){
+        
         if (!currentTasks.isEmpty) {
             return
         }
-
+        // note: we expect the query to only return unfinishedTasks
+        var unfinishedTasks: [Task] = []//=  TODO: makeQuery()
+        
+        while unfinishedTasks.isEmpty {
+            sleep(1)
+            
+            //unfinishedTasks = TODO: makeQuery()
+        }
+        
+        selectTaskFromAvailableTasks(unfinishedTasks: unfinishedTasks)
+        
+    }
+    
+    
+    func selectTaskFromAvailableTasks(unfinishedTasks: [Task]){
         for task in unfinishedTasks {
             if (task.drone_id == nil) {
-
+                
                 // TODO: registerForTask(task.id, checkTaskResponsibility)
-
+                
                 //TODO: startTask(task)  // api call to drone team
-
+                currentTasks.append(task)
+                
                 return
             }
         }
     }
-
+    
     /**
      expected parameters:
      -[{taskId, droneId, timestamp}] where taskId is always the same (the one we registered for)
-        --> we can filter for earliest timestamp, see if we currenlty to this task
+     --> we can filter for earliest timestamp, see if we currenlty to this task
      */
-    func checkTaskResponsibility(){
+    func checkTaskResponsibility(taskRegistrations: [TaskRegistration]){
+        
+        let earliestTaskRegistration = getEarliestTaskRegistration(taskRegistrations: taskRegistrations)
+        
+        if (earliestTaskRegistration.droneId == droneId){
+            // continue executing task
+            return
+        }
+        
+        // remove respective task from currentTasks when aborting
+        currentTasks = currentTasks.filter { task in task.id != earliestTaskRegistration.taskId }
+        
+        // TODO: abort task execution // api call to drone team
+        
     }
-
-	func parseJsonToTasks(json: String) -> [Task] {
-            guard let data = json.data(using: .utf8) else {
-                return []
+    
+    func getEarliestTaskRegistration(taskRegistrations:[TaskRegistration]) -> TaskRegistration{
+        var earliestTaskRegistration: TaskRegistration = taskRegistrations[0]
+        for taskRegistration in taskRegistrations {
+            if (taskRegistration.timestamp < earliestTaskRegistration.timestamp){
+                earliestTaskRegistration = taskRegistration
             }
-            var unknown_tasks: [UnknownTask] = []
-            do {
-                unknown_tasks = try! JSONDecoder().decode([UnknownTask].self, from: data)
-            }
+        }
+        
+        return earliestTaskRegistration
+    }
+    
+    
+    // json parsing
+    func parseJsonToTasks(json: String) -> [Task] {
+        guard let data = json.data(using: .utf8) else {
+            return []
+        }
+        var unknown_tasks: [UnknownTask] = []
+        do {
+            unknown_tasks = try! JSONDecoder().decode([UnknownTask].self, from: data)
+        }
+        var tasks: [Task] = []
+        for unknown_task in unknown_tasks {
+            tasks.append(parseTask(unknown: unknown_task))
+        }
+        
+        return tasks
+    }
+    
+    func parseTask(unknown: UnknownTask) -> Task {
+        let id: String = unknown.id
+        let name: String = unknown.name
+        let type: TaskType = unknown.type
+        let drone_id: String = unknown.drone_id
+        let task: Task
+        
+        switch type {
+        case .FlyToTask:
+            task = FlyToTask(id: id, name: name, type: type, drone_id: drone_id,
+                             latitude: unknown.latitude ?? 0,
+                             longitude: unknown.longitude ?? 0,
+                             altitude: unknown.altitude ?? 0)
+            
+        case .GetDataTask:
+            task = GetDataTask(id: id, name: name, type: type, drone_id: drone_id)
+            
+        case .NonTerminalTask:
             var tasks: [Task] = []
-            for unknown_task in unknown_tasks {
-                tasks.append(parseTask(unknown: unknown_task))
-            }
-
-            return tasks
-        }
-
-        func parseTask(unknown: UnknownTask) -> Task {
-            let id: String = unknown.id
-            let name: String = unknown.name
-            let type: TaskType = unknown.type
-            let drone_id: String = unknown.drone_id
-            let task: Task
-
-            switch type {
-            case .FlyToTask:
-                task = FlyToTask(id: id, name: name, type: type, drone_id: drone_id,
-                        latitude: unknown.latitude ?? 0,
-                        longitude: unknown.longitude ?? 0,
-                        altitude: unknown.altitude ?? 0)
-
-            case .GetDataTask:
-                task = GetDataTask(id: id, name: name, type: type, drone_id: drone_id)
-
-            case .NonTerminalTask:
-                var tasks: [Task] = []
-                if let unknown_tasks: [UnknownTask] = unknown.tasks {
-                    for unknown_task in unknown_tasks {
-                        tasks.append(parseTask(unknown: unknown_task))
-                    }
-                } else {
-                    tasks = []
+            if let unknown_tasks: [UnknownTask] = unknown.tasks {
+                for unknown_task in unknown_tasks {
+                    tasks.append(parseTask(unknown: unknown_task))
                 }
-                task = NonTerminalTask(id: id, name: name, type: type, drone_id: drone_id, tasks: tasks)
+            } else {
+                tasks = []
             }
-            return task
+            task = NonTerminalTask(id: id, name: name, type: type, drone_id: drone_id, tasks: tasks)
         }
+        return task
+    }
 }
