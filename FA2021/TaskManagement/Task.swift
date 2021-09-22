85f1b158818b77f094f5fb7bdd8522ae2fef362c
schusterbenjamin <benjamin.schuster.dorfen@web.de>
2021-09-22 11:46:54 +0200

    adding constructors and some thoughts

--- FA2021/TaskManagement/Task.swift
+++ FA2021/TaskManagement/Task.swift
@@ -1,15 +1,39 @@
 class Task {
-	let id: String
-	let name: String
-	let type: TaskType
+    
+    let id: String
+    let name: String
+    let type: TaskType
+    let drone_id: String
+    
+    internal init(id: String, name: String, type: TaskType, drone_id: String) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.drone_id = drone_id
+    }
 }
 
 class FlyToTask:Task {
-	let latitude: double
-	let longitude: double
-	let altitude: double
+    let latitude: Double
+    let longitude: Double
+    let altitude: Double
+    
+    internal init(id: String, name: String, type: TaskType, drone_id: String, latitude: Double, longitude: Double, altitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+        super.init(id: id, name: name, type: type, drone_id: drone_id)
+    }
+}
+
+class GetDataTask: Task {
 }
 
 class NonTerminalTask:Task {
-    task_ids: [String]
+    internal init(id: String, name: String, type: TaskType, drone_id: String, tasks: [Task]) {
+        self.tasks = tasks
+        super.init(id: id, name: name, type: type, drone_id: drone_id)
+    }
+    
+    let tasks: [Task]
 }
--- FA2021/TaskManagement/TaskManager.swift
+++ FA2021/TaskManagement/TaskManager.swift
@@ -4,17 +4,24 @@ enum TaskType {
 
 struct TaskManager {
 
+    
+    /**
+        @return tasks waiting to be assigned
+     */
     func retrieveAvailableTasks() -> [Task] {
-	 return [Task()];
+        // allTasksAvilableOrInProgress   coaty?: get/observe all tasks in network
+        // dronesWorkingOnTaskSinceTimestamp    coaty? (if not coaty add to json): get all drones that work on task with timestamp
+        // filter allTasksAvilableOrInProgress with dronesWorkingOnTaskSinceTimestamp
+        return [];
 	}
 
-    
-    func aquireTask(tasks: [Task]){
-        
-        for task in tasks {
+    /**
+     gets called while tasks available
+     */
+    func aquireTask(){
+        let availableTask = retrieveAvailableTasks()
+        for task in availableTask {
             
         }
-        
     }
-    
 }
