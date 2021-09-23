e22265617ce07c22436809052421b01f942f8648
jakob.scheffels <jakob.scheffels@tum.de>
2021-09-23 17:54:53 +0200

    Network Setup

---
+++ FA2021/Network/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  FA2021
+//
+//  Created by Ferienakademie on 23.09.21.
+//
+
+import Foundation
--- FA2021/Network/Network.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Network.swift
-//  FA2021
-//
-//  Created by Ferienakademie on 22.09.21.
-//
-
-import Foundation
