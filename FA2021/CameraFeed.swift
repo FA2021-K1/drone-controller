550065f14450c32473027ce736629ba6d41e84be
Nathalie Galla <nathalie.m.galla@gmail.com>
2021-09-23 11:15:47 +0200

    added a way to fly straight ahead, and the ability to create a waypoint mission to a given coordinate

--- FA2021/CameraFeed.swift
+++ FA2021/CameraFeed.swift
@@ -7,21 +7,20 @@
 
 import Foundation
 import DJISDK
-import DJIWidget
 
-class CameraFeed : NSObject {
-    
-    let product = DJISDKManager.product()
-    let cam : DJICamera
-    let feeder : DJIVideoFeeder
-    
-    override init() {
-        cam = (product?.camera)!
-    }
-    
-    func streamVideo() {
-        feeder.primaryVideoFeed
-    }
-}
+//class CameraFeed : NSObject {
+//
+//    let product = DJISDKManager.product()
+//    let cam : DJICamera
+//    let feeder : DJIVideoFeeder
+//
+//    override init() {
+//        cam = (product?.camera)!
+//    }
+//
+//    func streamVideo() {
+//        feeder.primaryVideoFeed
+//    }
+//}
 
 
