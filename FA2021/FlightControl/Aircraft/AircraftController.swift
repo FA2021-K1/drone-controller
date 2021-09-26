87a08de16f5e33da2165fa3457de38187770342e
Kevin Huang <kevin.huang@tum.de>
2021-09-26 21:14:57 +0200

    Add documentation references, add altitude and GPS level information

--- FA2021/FlightControl/Aircraft/AircraftController.swift
+++ FA2021/FlightControl/Aircraft/AircraftController.swift
@@ -17,6 +17,11 @@ class AircraftController {
     
     private var droneConnectionCancellable: AnyCancellable?
     
+    /**
+     The current location of the aircraft as a coordinate. `nil` if the location is invalid.
+     
+     [DJI SDK Documentation](https://developer.dji.com/api-reference/ios-api/Components/FlightController/DJIFlightController_DJIFlightControllerCurrentState.html#djiflightcontroller_djiflightcontrollercurrentstate_aircraftlocation_inline)
+     */
     var aircraftPosition: CLLocationCoordinate2D? {
         get {
             guard let key = DJIFlightControllerKey(param: DJIFlightControllerParamAircraftLocation)
@@ -35,24 +40,56 @@ class AircraftController {
         }
     }
     
+    /**
+     Relative altitude of the aircraft relative to take off location, measured by the barometer, in meters.
+     
+     [DJI SDK Documentation](https://developer.dji.com/api-reference/ios-api/Components/FlightController/DJIFlightController_DJIFlightControllerCurrentState.html#djiflightcontroller_djiflightcontrollercurrentstate_altitude_inline)
+     */
+    var aircraftAltitude: Double? {
+        get {
+            guard let key = DJIFlightControllerKey(param: DJIFlightControllerParamAltitudeInMeters)
+            else {
+                log.add(message: "Cannot retrieve current altitude: Missing Controller Key")
+                return nil
+            }
+            
+            let value = DJISDKManager.keyManager()?.getValueFor(key)
+            guard let altitude = value?.value as? Double else {
+                log.add(message: "Cannot retrieve current altitude")
+                return nil
+            }
+            
+            return altitude
+        }
+    }
+    
+    /**
+     GPS signal levels, which are used to measure the signal quality.
+
+     [DJI SDK Documentation](https://developer.dji.com/api-reference/ios-api/Components/FlightController/DJIFlightController_DJIFlightControllerCurrentState.html#djiflightcontroller_djigpssignalstatus_inline)
+     */
+    var gpsSignalLevel: DJIGPSSignalLevel? {
+        get {
+            guard let key = DJIFlightControllerKey(param: DJIFlightControllerParamGPSSignalStatus)
+            else {
+                log.add(message: "Cannot retrieve current GPS Signal Level: Missing Controller Key")
+                return nil
+            }
+            
+            let value = DJISDKManager.keyManager()?.getValueFor(key)
+            guard let gpsSignalLevel = value?.value as? DJIGPSSignalLevel else {
+                log.add(message: "Cannot retrieve current GPS Signal Level")
+                return nil
+            }
+            
+            return gpsSignalLevel
+        }
+    }
+    
     init(log: Log, droneConnection: DroneConnectionManager) {
         self.log = log
         self.state = .onGround
         self.droneConnection = droneConnection
-    }
-    
-    func getCurrentDronePosition() -> CLLocationCoordinate2D? {
-        guard let key = DJIFlightControllerKey(param: DJIFlightControllerParamAircraftLocation)
-        else {
-            log.add(message: "Missing controller key")
-            return nil;
-        }
-        let value = DJISDKManager.keyManager()?.getValueFor(key)
-        
-        guard let location = value?.value as? CLLocation else {
-            return nil
-        }
-        return location.coordinate
     }
     
     func takeOff() {
