c1884ad6a021630184d383e6b648d54092a49b1b
Ole <ole.petersen@go4more.de>
2021-09-28 21:31:58 +0200

    Added ip and port configuration to app UI

--- FA2021/ContentView.swift
+++ FA2021/ContentView.swift
@@ -12,13 +12,82 @@ import Combine
 struct ContentView: View {
     
     @ObservedObject private var viewModel = ViewModel()
+    @State private var showingInvalidPortAlert = false
+    @State private var coatyStarted = false
+    @State private var ip: String = ""
+    @State private var portString: String = ""
+    let defaults = UserDefaults.standard
     
     var body: some View {
+        
+        
         Text("Drone Controls")
             .fontWeight(.semibold)
             .foregroundColor(Color.init(red: 0, green: 101, blue: 189))
+            .font(.title)
+        
+        if coatyStarted {
+            HStack {
+                Text("IP: 111.222.333.444").font(.subheadline)
+                Text("Port: 1234").font(.subheadline)
+            }
+        }
         
         Divider()
+        
+        if !coatyStarted {
+            VStack {
+                VStack {
+                    let width: CGFloat = 50
+                    HStack {
+                        Text("IP:").frame(width: width, alignment: .center)
+                        TextField("123.456.789.123", text: $ip)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    HStack {
+                        Text("Port:").frame(width: width, alignment: .center)
+                        TextField("1234", text: $portString)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    
+                }
+                Button {
+                    if let port: UInt16 = UInt16(portString) {
+                        withAnimation {
+                            coatyStarted.toggle()
+                        }
+                        
+                        defaults.set(ip, forKey: "coaty_ip")
+                        defaults.set(String(port), forKey: "coaty_port")
+                        
+                        /*
+                         Potentialy the same iPhone could control different drones, meaning that the uuid of the iPhone might not always refer to the same drone.
+                         In our use case, each drone is assigned to one iPhone, so we can assume that the iPhones to not differ.
+                         */
+                        
+                        let coatyAPI: CoatyAPI = CoatyAPI(host_ip: ip, port: port)
+                        let firstComeFirstServe: TaskManager = FirstComeFirstServe(api: coatyAPI, droneId: UIDevice.current.identifierForVendor!.uuidString, taskContext: viewModel.flightControl.taskContext)
+
+                        // starts timer to send data in init()
+                        let _: Telemetry = Telemetry(api: coatyAPI, taskmanager: firstComeFirstServe)
+
+
+                        firstComeFirstServe.scanForTask()
+                        
+                    } else {
+                        showingInvalidPortAlert = true
+                    }
+                    
+                } label: {
+                    Text("Connect to Coaty").padding(20)
+                }.contentShape(Rectangle())
+                    .alert(isPresented: $showingInvalidPortAlert) {
+                        Alert(title: Text("Error"), message: Text("Port needs to be an integer!"))
+                    }
+            }
+            Divider()
+            
+        }
         
         // logger
         ScrollView {
@@ -53,15 +122,21 @@ struct ContentView: View {
                 Text("Land").padding(20)
             }.contentShape(Rectangle())
             
-            
             Button {
                 viewModel.flightControl.sampleTask()
             } label: {
                 Text("Sample Task").padding(20)
             }.contentShape(Rectangle())
+            
         }
         
         Divider()
+    }
+    
+    init() {
+        ip = defaults.string(forKey: "coaty_ip") ?? ""
+        portString = defaults.string(forKey: "coaty_port") ?? ""
+        print("Loaded ip: \(ip)")
     }
     
 }
@@ -87,23 +162,11 @@ extension ContentView {
             self.log = log
             self.flightControl = FlightControlService(log: log)
 
-                self.subscription = log.$logEntries.sink(receiveValue: { entries in
-                    self.logEntries = entries
-                })
-            
-            /*
-             Potentialy the same iPhone could control different drones, meaning that the uuid of the iPhone might not always refer to the same drone.
-             In our use case, each drone is assigned to one iPhone, so we can assume that the iPhones to not differ.
-             */
-            
-            let coatyAPI: CoatyAPI = CoatyAPI()
-            let firstComeFirstServe: TaskManager = FirstComeFirstServe(api: coatyAPI, droneId: UIDevice.current.identifierForVendor!.uuidString, taskContext: self.flightControl.taskContext)
-            
-            // starts timer to send data in init()
-            let _: Telemetry = Telemetry(api: coatyAPI, taskmanager: firstComeFirstServe)
+            self.subscription = log.$logEntries.sink(receiveValue: { entries in
+                self.logEntries = entries
+            })
             
             
-            firstComeFirstServe.scanForTask()
             
         }
 
